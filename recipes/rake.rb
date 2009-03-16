051bf2344889375e5d4b607bce172a720381a7a6
hiroshi <hiroshi3110@gmail.com>
2009-03-16 09:14:30 -0700

    add missing desc for the task

--- recipes/rake.rb
+++ recipes/rake.rb
@@ -1,4 +1,5 @@
 namespace :rake do
+  desc "TASK='rake tasks' (RAILS_ENV is cap's rails_env [default: 'production'])"
   task :default do
     rails_env = fetch(:rails_env, "production")
     run "(cd #{current_path}; RAILS_ENV=#{rails_env} rake #{ENV['TASK']})"
